3ec86bece95f4ff295820101e39594e8a7123963
Johan Dahlberg <dahlberg.johan@gmail.com>
2011-09-27 17:18:40 +0200

    comet test

--- cometbridge/server.js
+++ cometbridge/server.js
@@ -2,7 +2,12 @@ var createServer = require("http").createServer;
 var server;
 server = createServer(function(req, res) {
   var content = require("fs").readFileSync("iframe.html");
-  res.writeHead(200, { "content-Type": "text/html" });
-  res.end(content);
+  if (/^\/\?/.test(req.url)) {
+    res.writeHead(200, { "content-Type": "text/html" });
+    res.end(content);
+  } else {
+    res.writeHead(404, { "content-Type": "text/html" });
+    res.end("NOT_FOUND");
+  }
 });
 server.listen(8080);
